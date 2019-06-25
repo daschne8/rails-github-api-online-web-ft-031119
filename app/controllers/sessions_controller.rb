07cb36f5cb4b9b2623ba5fe55e407b81475c50ae
Daniel Schneider <daschne8@gmail.com>
2019-06-25 18:57:13 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
     end
     body = JSON.parse(response.body)
     session[:token] = body['access_token']
-
+    binding.pry
+    puts "done"
 
 
     redirect_to '/'
